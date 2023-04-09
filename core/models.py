8117b660f4a37e3b026b7c625d4322e8dadfa22f
tasnimferdous <tasnimferdous216@gmail.com>
2023-04-09 15:53:52 +0600

    User model modified and tested

--- core/models.py
+++ core/models.py
@@ -1,3 +1,51 @@
+"""
+Database models
+"""
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    """Manager for User"""
+    def create_user(self, email, password = None, **extra_fields):
+        """Create & return a new user"""
+        if not email:
+            raise ValueError('User must have an email')
+        user = self.model(email = self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using = self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and return superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using = self._db)
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system"""
+    user_roles = [
+        ('buyer', 'buyer'),
+        ('seller', 'seller'),
+    ]
+    email = models.EmailField(max_length=255, unique=True)
+    username = models.CharField(max_length=255, blank=True)
+    first_name = models.CharField(max_length=255, blank=True)
+    last_name = models.CharField(max_length=255, blank=True)
+    phone = models.CharField(max_length=255, blank=True)
+    address = models.CharField(max_length=255, blank=True)
+    works_at = models.CharField(max_length=255, blank=True)
+    role = models.CharField(max_length=255, choices=user_roles, default='buyer')
+    # avatar = models.ImageField()
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
